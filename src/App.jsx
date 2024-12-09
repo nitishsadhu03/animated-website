f0e9c223bc1bdf97ba85f91e7422f9be3a2274f6
Nitish Sadhu <nitishsadhu03@gmail.com>
2024-12-09 20:11:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import NavBar from "./components/Navbar";
+import Features from "./components/Features";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Story from "./components/Story";
+
+const App = () => {
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <NavBar />
+      <Hero />
+      <About />
+      <Features/>
+      <Story/>
+      <Contact/>
+      <Footer/>
+    </main>
+  );
+};
+
+export default App;
